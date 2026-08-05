a37908a5ecb1134f2d11bdf90fb40770b07eec9f
rmaratea-pdf <r.maratea@pharmadatafactory.com>
2026-08-05 13:26:12 +0200

    Setup main window #2

---
+++ FlipLoop/FlipLoop/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace FlipLoop;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Open_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog dlg = new();
+
+        dlg.Filter =
+            "Audio|*.wav;*.mp3";
+
+        if (dlg.ShowDialog() == true)
+        {
+            MessageBox.Show(
+                $"File selezionato:\n{dlg.FileName}");
+        }
+    }
+
+    private void Exit_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
